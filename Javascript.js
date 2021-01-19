1194eb3a62baf7d0bcc442bfcc5613f30acf704b
HenryVernon <75549827+HenryVernon@users.noreply.github.com>
2021-01-19 18:16:53 -0500

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js">
+
+document.getElementById("button1").addEventListener("click", function(){
+
+document.getElementById("box").style.height = "300px"
+
+});
+
+</script>
+
+
